2598ee9e80504d12c71c848e5ec721a6cfeb59fb
Enriquezwere <enriquez.marieleanne@gmail.com>
2023-10-22 10:56:33 -0800

    2ndFloorVTour

---
+++ data.js
@@ -0,0 +1,780 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-2nd-floor",
+      "name": "2nd Floor",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0589677808105122,
+          "pitch": 0.06640913710787721,
+          "rotation": 0,
+          "target": "1-hallway"
+        },
+        {
+          "yaw": -0.4430565558033095,
+          "pitch": 0.07862930893752385,
+          "rotation": 0,
+          "target": "3-hallway"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.0801426697344354,
+          "pitch": 0.15175296518612846,
+          "title": "2nd Floor",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "1-hallway",
+      "name": "Hallway",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "yaw": -1.7230531372922506,
+        "pitch": -0.053367753605629176,
+        "fov": 1.3563401604162861
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.680720750258823,
+          "pitch": 0.04016517781493434,
+          "rotation": 0,
+          "target": "2-hallway"
+        },
+        {
+          "yaw": -0.4340087449504555,
+          "pitch": 0.13449650595243767,
+          "rotation": 0,
+          "target": "0-2nd-floor"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-hallway",
+      "name": "Hallway",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.738245507485937,
+          "pitch": 0.08422099129816019,
+          "rotation": 0,
+          "target": "1-hallway"
+        },
+        {
+          "yaw": -0.0680266126275022,
+          "pitch": 0.08748775561318212,
+          "rotation": 0,
+          "target": "10-hallway"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-hallway",
+      "name": "Hallway",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.676897040433319,
+          "pitch": 0.1099530820781851,
+          "rotation": 0,
+          "target": "0-2nd-floor"
+        },
+        {
+          "yaw": 1.0467901608099037,
+          "pitch": 0.07537562669144471,
+          "rotation": 0,
+          "target": "4-hallway"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-hallway",
+      "name": "Hallway",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.004633419020503737,
+          "pitch": 0.13060225878221132,
+          "rotation": 0,
+          "target": "5-grade-11-faculty"
+        },
+        {
+          "yaw": 3.076017050222413,
+          "pitch": 0.12832674188722493,
+          "rotation": 0,
+          "target": "3-hallway"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-grade-11-faculty",
+      "name": "Grade 11 Faculty",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1296150650412944,
+          "pitch": 0.13923239393579223,
+          "rotation": 0,
+          "target": "4-hallway"
+        },
+        {
+          "yaw": 1.625727955452244,
+          "pitch": 0.1283608415855646,
+          "rotation": 0,
+          "target": "6-comfort-room"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.3375964147792665,
+          "pitch": -0.35036645120860577,
+          "title": "Grade 11 Advisers",
+          "text": "Grade 12 advisers are educators who assist and guide senior high school students in their academic and post-graduation planning.<div><br></div>"
+        },
+        {
+          "yaw": -0.6798459801642629,
+          "pitch": 0.08606100909778647,
+          "title": "Faculty Room",
+          "text": "Teacher faculty room is a designated workspace within an educational institution where teachers and faculty members can collaborate, prepare for lessons, grade assignments, and engage in professional discussions and activities.<div><br></div>"
+        }
+      ]
+    },
+    {
+      "id": "6-comfort-room",
+      "name": "Comfort Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "yaw": 0.4701703291086723,
+        "pitch": 0.2751347851747603,
+        "fov": 1.3563401604162861
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.4175691666151309,
+          "pitch": 0.5171038227352707,
+          "rotation": 0,
+          "target": "7-hallway"
+        },
+        {
+          "yaw": 0.018465964256371592,
+          "pitch": 0.16447893321455886,
+          "rotation": 0,
+          "target": "5-grade-11-faculty"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-hallway",
+      "name": "Hallway",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.004761404117816781,
+          "pitch": 0.1800896687039515,
+          "rotation": 0,
+          "target": "8-hallway"
+        },
+        {
+          "yaw": -1.7257479217977565,
+          "pitch": -0.21050290190035525,
+          "rotation": 0,
+          "target": "6-comfort-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-hallway",
+      "name": "Hallway",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.0660812400628874,
+          "pitch": 0.07298756731543676,
+          "rotation": 0,
+          "target": "9-hallway"
+        },
+        {
+          "yaw": -3.1132783551639402,
+          "pitch": 0.14256624791104855,
+          "rotation": 0,
+          "target": "7-hallway"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-hallway",
+      "name": "Hallway",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "yaw": -0.01852465368419054,
+        "pitch": 0.12539800078300978,
+        "fov": 1.3563401604162861
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5831794855209722,
+          "pitch": 0.07777468941108268,
+          "rotation": 0,
+          "target": "8-hallway"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-hallway",
+      "name": "Hallway",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.008836792792942916,
+          "pitch": 0.14045561825508202,
+          "rotation": 0,
+          "target": "11-hallway"
+        },
+        {
+          "yaw": -1.7366489998973904,
+          "pitch": 0.08846600374866576,
+          "rotation": 0,
+          "target": "2-hallway"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-hallway",
+      "name": "Hallway",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.502918406214258,
+          "pitch": 0.14216461575176353,
+          "rotation": 0,
+          "target": "10-hallway"
+        },
+        {
+          "yaw": -0.014140910075028046,
+          "pitch": 0.05214210771239891,
+          "rotation": 0,
+          "target": "12-hallway"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-hallway",
+      "name": "Hallway",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4718089309491162,
+          "pitch": 0.17617162455696445,
+          "rotation": 0,
+          "target": "13-library"
+        },
+        {
+          "yaw": -3.0655101947707664,
+          "pitch": 0.4139835281584556,
+          "rotation": 0,
+          "target": "11-hallway"
+        },
+        {
+          "yaw": 0.04596795133018716,
+          "pitch": 0.21277991188113,
+          "rotation": 0,
+          "target": "14-hallway-college"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-library",
+      "name": "Library",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.70455356605099,
+          "pitch": 0.13577531113459784,
+          "rotation": 0,
+          "target": "12-hallway"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.6299016118581875,
+          "pitch": 0.00697760336292319,
+          "title": "Library&nbsp;",
+          "text": "A library is a resourceful space in an educational institution or community, stocked with a wide variety of books, periodicals, and digital materials, accessible to individuals for reading, research, and academic or leisure pursuits.<div><br></div>"
+        }
+      ]
+    },
+    {
+      "id": "14-hallway-college",
+      "name": "Hallway (College)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -7.043610139589873e-10,
+          "pitch": 0.07643808539304331,
+          "rotation": 0,
+          "target": "15-hallway-college"
+        },
+        {
+          "yaw": -3.0237610641456776,
+          "pitch": 0.24337428817827345,
+          "rotation": 0,
+          "target": "12-hallway"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "15-hallway-college",
+      "name": "Hallway (College)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.03815869123064,
+          "pitch": 0.11455694597969313,
+          "rotation": 0,
+          "target": "16-comfort-room-college"
+        },
+        {
+          "yaw": -3.006653228126684,
+          "pitch": 0.11360610245950475,
+          "rotation": 0,
+          "target": "14-hallway-college"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "16-comfort-room-college",
+      "name": "Comfort Room (College)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.011682497306303,
+          "pitch": 0.11829346506772787,
+          "rotation": 0,
+          "target": "15-hallway-college"
+        },
+        {
+          "yaw": 2.3058043193296704,
+          "pitch": 0.09577125384909024,
+          "rotation": 0,
+          "target": "14-hallway-college"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "2nd Floor",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
